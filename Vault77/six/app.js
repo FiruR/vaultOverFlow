9542d903ec7472d499b481bce192fa77a390c8c3
Firuza Raiymkul <raiymkulfiruza@gmail.com>
2019-10-29 08:59:02 +0600

    hotfix(): while

--- Vault77/six/app.js
+++ Vault77/six/app.js
@@ -1,11 +1,36 @@
 let passw = 'mypassword'
 let msg = "введите пароль"
+console.log(passw, 'passwsdd')
 let prom = prompt(msg);
-console.log(passw, 'passw')
+let i = 4;
 
-if (prom == passw) {
-	document.write('Секретная информация');
-} else if (prom !== passw) {
-	alert('Неправильный пароль')
-	prompt(msg);
-} 
+
+
+// if (prom !== passw) {
+// 	for(let i=2; i>=0; i--) {
+// 	console.log(i);
+// 	console.log(prom);
+// 		console.log(prom);
+// 		if (i<1) {
+// 			document.write('Вы заблокированы')
+// 		} else {
+// 			alert(`Неправильный пароль. У вас осталось ${i} попыток`)
+// 			prompt(msg);
+// 		}
+// 	}
+// }
+
+while (i !== 0) {
+	if (prom !== passw) {
+			alert(`Неправильный пароль. У вас осталось ${i} попыток`)
+			prompt(msg);
+		i--;
+	} else if (prom == passw) {
+		alert('welcome')
+	}
+}
+
+
+
+
+
