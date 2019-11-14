b9b76c730e81ef9cd4630b76e13ce6bdcd244a43
Firuza Raiymkul <raiymkulfiruza@gmail.com>
2019-11-14 21:00:12 +0600

    hotfix(): title, description, date

--- Vault77/vaultOverFlow/app.js
+++ Vault77/vaultOverFlow/app.js
@@ -5,9 +5,19 @@ let request = fetch(`http://localhost:1337/question`);
 request.then(function(response){
 	const jsonResp = response.json();
 	jsonResp.then(function(data){
+		date = new Date();
 		for(let i= 0; i< data.length; i++) {
-			document.write(data[i].title)
-			console.log('data',data[i].title);
+
+			let dateCreat = data[i].createdAt;
+			date = new Date(dateCreat);
+			day = date.getDay();
+			month = date.getMonth();
+			year = date.getFullYear();
+
+			console.log(data[i].description)
+
+			let title = document.getElementById('title').innerHTML += `<br> <h3> ${i}.${data[i].title}. </h3> 
+			Создано: ${day}.${month}.${year} <br> ${data[i].description}`;
 		}
 	})
 })
